5c05ddc52ad92b547c7d5334c6012fb0837c948b
Rajkumar <108941795+Rajedyoda@users.noreply.github.com>
2023-05-31 15:26:11 +0530

    Student_management
    Create a Student Management Application based on the conditions specified below

---
+++ Student_management.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE Studentmanagement;
+
+USE Studentmanagement;
+
+create table Student(
+Student_name varchar(255),
+Roll_no int(50),
+Dob date
+);
+
+DESC Student;
+
+insert into Student_name values 
+('Krish',0101,'1988-11-12'),
+('Rajkumar',0102,'1985-05-31'),
+('Vignesh',0103,'1986-05-20'),
+('Vaishnavi',0104,'14-12-11');
+
